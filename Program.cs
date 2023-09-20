a486bd4708666d7a3de0e4e2ede75e6941bbf63c
Łukasz Adamczyk <lukaszadamczy@gmail.com>
2023-09-20 07:39:24 +0100

    Feature: Counting repeated characters by linq #1
    Another implementation of the already existing method
    CountRepeatedCharacters, but this time done only in LINQ.

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace CharacterCounter
         /// </summary>
         /// <param name="str"></param>
         /// <returns></returns>
-        public static int CountCharacters(string str)
+        public static int CountRepeatedCharacters(string str)
         {
             str = str.ToLower();
             string charactersChecked = "";
@@ -33,16 +33,30 @@ namespace CharacterCounter
 
             return output;
         }
+
+        /// <summary>
+        /// Counts how many characters are repeated. For eg. "aabbcdef" is 2, because 'a' and 'b' is repeated
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static int CountRepeatedCharactersLINQ(string str) => str.GroupBy(n => n).Where(g => g.Count() > 1).Count();
     }
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(CharacterCounter.CountCharacters("aaBBcdefg")); //2
-            Console.WriteLine(CharacterCounter.CountCharacters("Vincinty")); //2
-            Console.WriteLine(CharacterCounter.CountCharacters("aaBBccdd")); //4
-            Console.WriteLine(CharacterCounter.CountCharacters("abcdefg")); //0
+            Console.WriteLine(CharacterCounter.CountRepeatedCharacters("aaBBcdefg")); //2
+            Console.WriteLine(CharacterCounter.CountRepeatedCharacters("Vicinity")); //1
+            Console.WriteLine(CharacterCounter.CountRepeatedCharacters("aaBBccdd")); //4
+            Console.WriteLine(CharacterCounter.CountRepeatedCharacters("abcdefg")); //0
+
+            Console.WriteLine("------------");
+
+            Console.WriteLine(CharacterCounter.CountRepeatedCharactersLINQ("aaBBcdefg")); //2
+            Console.WriteLine(CharacterCounter.CountRepeatedCharactersLINQ("Vicinity")); //1
+            Console.WriteLine(CharacterCounter.CountRepeatedCharactersLINQ("aaBBccdd")); //4
+            Console.WriteLine(CharacterCounter.CountRepeatedCharactersLINQ("abcdefg")); //0
 
         }
     }

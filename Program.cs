758e2016d472d62bf12c4a09b755ef86ab312c61
Łukasz Adamczyk <lukaszadamczy@gmail.com>
2023-09-20 07:03:08 +0100

    Initialize VS project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CharacterCounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
